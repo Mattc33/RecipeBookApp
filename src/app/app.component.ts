8026ac8bcc74663a3b523043447e2e6a4df6e585
Matthew Chan <mattc3303@gmail.com>
2018-11-15 14:58:00 -0500

    project structure setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,4 @@ import { Component } from '@angular/core'
 })
 export class AppComponent {
     
-	title: string = 'test title'
-	
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './Containers/header/header.component'
+
+import { RecipeBookComponent } from './Containers/recipe-book/recipe-book.component'
+    import { RecipeDetailComponent } from './Components/recipes/recipe-detail/recipe-detail.component'
+    import { RecipeListComponent } from './Components/recipes/recipe-list/recipe-list.component'
+        import { RecipeItemComponent } from './Components/recipes/recipe-list/recipe-item/recipe-item.component'
+
+import { ShoppingListComponent } from './Containers/shopping-list/shopping-list.component'
+    import { ShoppingEditComponent } from './Components/shopping/shopping-edit/shopping-edit.component'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        ShoppingListComponent,
+        RecipeBookComponent,
+        RecipeDetailComponent,
+        RecipeListComponent,
+        RecipeItemComponent,
+        ShoppingEditComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule
+    ],
+    providers: [
 
-  ],
-  bootstrap: [AppComponent]
+    ],
+    bootstrap: [
+        AppComponent
+    ]
 })
 export class AppModule { }
